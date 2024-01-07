c27adf46b1b089cf0bf6e4151aa1ec3bd3202dda
Ejako <emjako@gmail.com>
2024-01-07 14:40:11 +0100

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  7 14:34:31 2024
+
+@author: Emil Jakobsen
+"""
+
+## Data handling
+import pandas as pd
+import numpy as np
+import statistics
+
+## SQL
+from sqlalchemy import create_engine
+
+## Data
+import yfinance as yf
+
+## UI
+import streamlit as st
+
+engine = create_engine('sqlite:///Test.db')
+
+df = yf.download('MMM', start = '2020-01-01')
+
+df.to_sql(df, engine)
+
+df2 = pd.read_sql('MMM', engine)
+
+st.dataframe(df2.head())
