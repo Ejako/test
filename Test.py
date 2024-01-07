37908964dbacf5b9dfe1d446c61c846272a33a9f
Ejako <emjako@gmail.com>
2024-01-07 16:53:54 +0100

    Add files via upload

--- Test.py
+++ Test.py
@@ -20,12 +20,28 @@ import yfinance as yf
 ## UI
 import streamlit as st
 
-
-df = yf.download('MMM', start = '2020-01-01')
+stock = yf.Ticker('MMM')
+df = stock.history('MMM', start = '2020-01-01')
+df.reset_index(inplace=True)
 
 conn = sqlite3.connect('test.db')
-df.to_sql(df, conn, if_exists = 'replace')
+c = conn.cursor()
 
-df2 = pd.read_sql('MMM', conn)
+c.execute('CREATE TABLE IF NOT EXISTS MMM (Date INTEGER, Open INTEGER, High INTEGER, Low INTEGER, Close INTEGER, Volume INTEGER, Dividends INTEGER, "Stock Splits" INTEGER);')
+
+for index, row in df.iterrows():
+    c.execute('INSERT INTO "MMM" (Date, Open, High, Low, Close, Volume, Dividends, "Stock Splits") VALUES (:date, :open, :high, :low, :close, :volume, :dividends, :splits);', 
+              {'date': str(row['Date']), 'open': row['Open'], 'high': row['High'], 'low': row['Low'], 'close': row['Close'], 'volume': row['Volume'], 'dividends': row['Dividends'], 'splits': row['Stock Splits']})
+
+conn.commit()
+
+df2 = pd.read_sql('SELECT * FROM MMM', conn).set_index('Date')
+
+conn.close()
+
+
+df2.index = pd.to_datetime(df2.index)
+
+print(df2.head())
 
 st.dataframe(df2.head())
