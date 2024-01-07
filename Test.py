7c74ea2673d6670d4e181e90c4605c396c6e9a08
Ejako <emjako@gmail.com>
2024-01-07 18:38:58 +0100

    Add files via upload

--- Test.py
+++ Test.py
@@ -20,25 +20,36 @@ import yfinance as yf
 ## UI
 import streamlit as st
 
-stock = yf.Ticker('MMM')
-df = stock.history('MMM', start = '2020-01-01')
+def update_sql(df, ticker):
+    conn = sqlite3.connect('test.db')
+    c = conn.cursor()
+    
+    c.execute(f'CREATE TABLE IF NOT EXISTS "{ticker}" (Date INTEGER, Open INTEGER, High INTEGER, Low INTEGER, Close INTEGER, Volume INTEGER, Dividends INTEGER, "Stock Splits" INTEGER);')
+    
+    for index, row in df.iterrows():
+        c.execute(f'INSERT INTO "{ticker}" (Date, Open, High, Low, Close, Volume, Dividends, "Stock Splits") VALUES (:date, :open, :high, :low, :close, :volume, :dividends, :splits);', 
+                  {'date': str(row['Date']), 'open': row['Open'], 'high': row['High'], 'low': row['Low'], 'close': row['Close'], 'volume': row['Volume'], 'dividends': row['Dividends'], 'splits': row['Stock Splits']})
+
+    conn.commit()
+    conn.close()
+    
+
+def retrieve_df(ticker):
+    conn = sqlite3.connect('test.db')
+    
+    df = pd.read(f'SELECT * FROM "{ticker}"', conn)
+    
+    conn.close()
+    
+    return df
+
+stock = yf.Ticker('JNJ')
+df = stock.history('JNJ', start = '2020-01-01')
 df.reset_index(inplace=True)
 
-conn = sqlite3.connect('test.db')
-c = conn.cursor()
+update_sql(df)
 
-c.execute('CREATE TABLE IF NOT EXISTS MMM (Date INTEGER, Open INTEGER, High INTEGER, Low INTEGER, Close INTEGER, Volume INTEGER, Dividends INTEGER, "Stock Splits" INTEGER);')
-
-for index, row in df.iterrows():
-    c.execute('INSERT INTO "MMM" (Date, Open, High, Low, Close, Volume, Dividends, "Stock Splits") VALUES (:date, :open, :high, :low, :close, :volume, :dividends, :splits);', 
-              {'date': str(row['Date']), 'open': row['Open'], 'high': row['High'], 'low': row['Low'], 'close': row['Close'], 'volume': row['Volume'], 'dividends': row['Dividends'], 'splits': row['Stock Splits']})
-
-conn.commit()
-
-df2 = pd.read_sql('SELECT * FROM MMM', conn).set_index('Date')
-
-conn.close()
-
+df2 = retrieve_df('JNJ')
 
 df2.index = pd.to_datetime(df2.index)
 
